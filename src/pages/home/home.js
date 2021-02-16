0f6a4bb0f87297f710c8b50a4eb6ecf75673a6ed
Joaquin Pato Decima <joaquin.decima@gmail.com>
2021-02-16 03:11:29 -0300

    Add Date

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,6 +3,7 @@ import API from '../../services/api.js';
 
 import './home.css';
 import CurrentTemp from '../../components/currenttemp/currenttemp.js';
+import DateFecha from '../../components/date/date.js';
 import Humidity from '../../components/humidity/humidity.js';
 import MinMax from '../../components/minmax/minmax.js';
 import WindSpeed from '../../components/windspeed/windspeed.js';
@@ -40,18 +41,19 @@ function Home() {
 
   return (
     <div className="container">
-      <div className="row" >
+      <div class="tiles-grid" >
 
         <CurrentTemp
           img={'http://openweathermap.org/img/wn/'.concat(data.weather[0].icon).concat('@2x.png')}
           city={data.name}
           temp={data.main.temp}
           description={data.weather[0].description.charAt(0).toUpperCase().concat(data.weather[0].description.slice(1))} />
+        <DateFecha />
+        <Humidity humidity={data.main.humidity} />
+        <WindSpeed viento={data.wind.speed} />
         <MinMax
           min={data.main.temp_min}
           max={data.main.temp_max} />
-        <Humidity humidity={data.main.humidity} />
-        <WindSpeed viento={data.wind.speed} />
       </div>
     </div>
   );

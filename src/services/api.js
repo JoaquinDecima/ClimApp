d779c4aa71580c77b23406c64ca7c9e2f6b7e1e4
Joaquin Pato Decima <joaquin.decima@gmail.com>
2021-02-16 12:20:57 -0300

    Add Nexts

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,11 @@ const server = 'http://181.31.84.158:3030/v1/';
 const API = {
   current: () => axios.get(`${server}current/`)
                     .then(response => response.data),
-  currentcity: city => axios.get(`${server}/current/${city}`)
+  currentcity: city => axios.get(`${server}current/${city}`)
+                    .then(response => response.data),
+  next: () => axios.get(`${server}forecast/`)
+                    .then(response => response.data),
+  nextcity: city => axios.get(`${server}forecast/${city}`)
                     .then(response => response.data)
 };
 

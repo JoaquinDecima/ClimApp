dfc7f493697160525a4408e0141f7dc4f9f29b85
Joaquin Pato Decima <joaquin.decima@gmail.com>
2021-02-16 01:15:02 -0300

    Add Current Temp

---
+++ src/components/currenttemp/currenttemp.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './currenttemp.css';
+
+function CurrentTemp(props) {
+
+  return (
+    <div className="cell-md-6 cli-card">
+      <div className="container-fluid">
+        <div className="row">
+
+          <div className="cell-md-12">
+            <h2><span class="mif-location"></span> {props.city}</h2>
+          </div>
+
+          <div className="cell-md-4">
+            <div className="img-container">
+              <img src={props.img} />
+            </div>
+          </div>
+
+          <div className="cell-md-8 text-center">
+            <span className="temp">{Math.trunc(props.temp)}<span class="mif-celsius"></span></span>
+          </div>
+
+          <div className="cell-md-12 text-center">
+            {props.description}
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default CurrentTemp;

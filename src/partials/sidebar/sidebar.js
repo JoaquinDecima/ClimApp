79ffa9c916a0986fc4e90433f4f2708ee8ac2587
Joaquin Pato Decima <joaquin.decima@gmail.com>
2021-02-16 01:14:31 -0300

    Fix Sidebar

--- src/partials/sidebar/sidebar.js
+++ src/partials/sidebar/sidebar.js
@@ -32,16 +32,26 @@ function SideBar() {
 
       <ul className="sidebar-menu">
         <li className="group-title">Sobre Mi</li>
-        <li><a target="_blank" href="https://joaquindecima.gitlab.io/"><span className="mif-home icon"></span>Mi Sitio</a></li>
-        <li><a target="_blank" href="https://www.linkedin.com/in/joaquindecima/"><span className="mif-linkedin icon"></span>Linkedin</a></li>
-
+        <li><a target="_blank" rel="noreferrer" href="https://joaquindecima.gitlab.io/">
+          <span className="mif-home icon"></span>Mi Sitio
+        </a></li>
+        <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/joaquindecima/">
+          <span className="mif-linkedin icon"></span>Linkedin
+        </a></li>
         <li className="group-title">Otros</li>
-        <li><a target="_blank" href="https://patojad.com.ar/"><span className="mif-blogger icon"></span>Blog</a></li>
-
+        <li><a target="_blank" rel="noreferrer" href="https://patojad.com.ar/">
+          <span className="mif-blogger icon"></span>Blog
+        </a></li>
         <li className="group-title">Contacto</li>
-        <li><a target="_blank" href="https://t.me/PatoJAD"><span className="mif-paper-plane icon"></span>Telegram</a></li>
-        <li><a target="_blank" href="https://api.whatsapp.com/send?phone=541160109950&text=Gracias%20por%20comunicarte.%20%C2%BFEn%20que%20puedo%20ayudarte%3F"><span className="mif-whatsapp icon"></span>WhatsApp</a></li>
-        <li><a target="_blank" href="mailto:joaquin.decima@gmail.com"><span className="mif-mail icon"></span>Email</a></li>
+        <li><a target="_blank" rel="noreferrer" href="https://t.me/PatoJAD">
+          <span className="mif-paper-plane icon"></span>Telegram
+        </a></li>
+        <li><a target="_blank" rel="noreferrer" href="https://api.whatsapp.com/send?phone=541160109950&text=Gracias%20por%20comunicarte.%20%C2%BFEn%20que%20puedo%20ayudarte%3F">
+          <span className="mif-whatsapp icon"></span>WhatsApp
+        </a></li>
+        <li><a target="_blank" rel="noreferrer" href="mailto:joaquin.decima@gmail.com">
+          <span className="mif-mail icon"></span>Email
+        </a></li>
       </ul>
     </aside>
   );

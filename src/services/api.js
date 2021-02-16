2cb6256318ccc736ef16c9b292a1d652fd41d70f
Joaquin Pato Decima <joaquin.decima@gmail.com>
2021-02-16 01:14:37 -0300

    Init API

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const server = 'http://181.31.84.158:3030/v1/';
+
+const API = {
+  current: () => axios.get(`${server}current/`)
+                    .then(response => response.data),
+  currentcity: city => axios.get(`${server}/current/${city}`)
+                    .then(response => response.data)
+};
+
+export default API;

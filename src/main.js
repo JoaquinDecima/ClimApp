59ad06911f2b84c9feff7b6efc526e1fdc363225
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-07 19:53:30 -0300

    clear main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { current, next } from "./service/api.js";
 
 const app = createApp(App)
 
 app.mount('#app')
-app.config.globalProperties.$currentWeatherData = await current()

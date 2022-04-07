742c6e2166ea3ac81b376bdae2d1112964a2a90e
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-07 09:53:42 -0300

    Add Cities examples

--- src/components/modules/Cities.vue
+++ src/components/modules/Cities.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="col-12">
-    <select class="form-select" aria-label="Default select example">
-      <option selected>Open this select menu</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
+  <div class="col-12 cities">
+    <select class="form-select text-center" aria-label="Default select example">
+      <option selected>Seleccione otra ciudad</option>
+      <option value="Buenos%20Aires">Buenos Aires</option>
+      <option value="Tucuman">Tucuman</option>
+      <option value="Mendoza">Mendoza</option>
+      <option value="Entre%20Rios">Entre Rios</option>
+      <option value="Cordoba">Cordoba</option>
+      <option value="Rio%20Negro">Rio Negro</option>
+      <option value="Salta">Salta</option>
+      <option value="Chaco">Chaco</option>
+      <option value="Santiago%20del%20Estero">Santiago del Estero</option>
+      <option value="La%20Rioja">La Rioja</option>
     </select>
   </div>
 </template>
@@ -16,5 +23,19 @@ export default {
 </script>
 
 <style scoped>
+  .cities{
+    padding: 20px;
+    border-radius: 12px;
+    background-color: rgba(20,20,20,0.6) !important;
+    backdrop-filter: blur(6px);
+    margin: 15px 0px;
+  }
 
+  select{
+    border-radius: 12px;
+    color: white;
+    background: rgba(20,20,20,0.6);
+    backdrop-filter: blur(6px);
+    border-color: #74D9B1;
+  }
 </style>

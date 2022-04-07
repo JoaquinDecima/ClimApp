81cc8db2cfe844801c1891421496c5c30107c202
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-07 11:26:30 -0300

    Add basic table

---
+++ src/components/modules/NexDaysTable.vue
@@ -0,0 +1,43 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr class="text-center">
+        <th scope="col"><i class="fa fa-calendar" aria-hidden="true"></i></th>
+        <th scope="col"><i class="fa fa-cloud" aria-hidden="true"></i></th>
+        <th scope="col"><i class="fa fa-thermometer-empty" aria-hidden="true"></i></th>
+        <th scope="col"><i class="fa fa-thermometer-full" aria-hidden="true"></i></th>
+        <th scope="col"><i class="fa fa-angle-double-right" aria-hidden="true"></i></th>
+        <th scope="col"><i class="fa fa-tint" aria-hidden="true"></i></th>
+      </tr>
+    </thead>
+    <tbody>
+    <NextDaysRow/><NextDaysRow/><NextDaysRow/><NextDaysRow/><NextDaysRow/>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import NextDaysRow from "./NextDaysRow.vue";
+export default {
+  name: "NexDaysTable",
+  components: {NextDaysRow}
+}
+</script>
+
+<style scoped>
+  table{
+    border-radius: 12px;
+    background-color: rgba(20,20,20,0.6) !important;
+    backdrop-filter: blur(6px);
+    color: white;
+  }
+
+  thead{
+    color: #74D9B1;
+    border-bottom-color: #74D9B1;
+  }
+
+  tbody{
+    border: 0px !important;
+  }
+</style>
---
+++ src/components/modules/NextDaysRow.vue
@@ -0,0 +1,41 @@
+<template>
+  <tr class="text-center align-middle">
+    <th scope="row">
+      <p class="day">25</p>
+      <p class="month">Septiembre</p>
+    </th>
+    <td>Mark</td>
+    <td>Otto</td>
+    <td>@mdo</td>
+    <td>Otto</td>
+    <td>@mdo</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "NextDaysRow"
+}
+</script>
+
+<style scoped>
+  th, td{
+    border-bottom: 0px;
+    border-top: 1px solid #74D9B1;
+  }
+
+  th{
+    color: #74D9B1;
+  }
+
+  .day{
+    font-size: 23px;
+    margin-bottom: 0px;
+    padding-bottom: 0px;
+  }
+
+  .month{
+    margin-top: 0px;
+    padding-top: 0px;
+  }
+</style>

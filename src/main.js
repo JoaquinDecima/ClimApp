9e08c64a61894cf8aae70b15d52dfb5ed693d4cf
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-07 09:53:17 -0300

    Add globals variables

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { current, next } from "./service/api.js";
 
 const app = createApp(App)
 
 app.mount('#app')
+app.config.globalProperties.$currentWeatherData = await current()
+app.config.globalProperties.$nextWeatherData = await next()
+
+console.log(app.config.globalProperties.$nextWeatherData)
+console.log(app.config.globalProperties.$currentWeatherData)

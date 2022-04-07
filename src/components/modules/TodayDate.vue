f6c6377c30fe78dde3d07069abaff9f18684371a
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-07 11:26:41 -0300

    Add TodayDate.vue

---
+++ src/components/modules/TodayDate.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="col-md-6 text-center today">
+    <p class="day">{{weekday}} {{day}}</p>
+    <p class="date">{{month}} de {{year}}</p>
+  </div>
+</template>
+
+<script>
+const date = new Date();
+const year = new Intl.DateTimeFormat('es', { year: 'numeric' }).format(date);
+const month = new Intl.DateTimeFormat('es', { month: 'long' }).format(date);
+const day = new Intl.DateTimeFormat('es', { day: '2-digit' }).format(date);
+const weekday = new Intl.DateTimeFormat('es', { weekday: "long" }).format(date);
+export default {
+  name: "TodayDate",
+  data: () => {
+    return{
+      year,
+      month: month.charAt(0).toUpperCase().concat(month.slice(1)),
+      day,
+      weekday: weekday.charAt(0).toUpperCase().concat(weekday.slice(1))
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .today{
+    border-radius: 12px;
+    background-color: rgba(20,20,20,0.6) !important;
+    backdrop-filter: blur(6px);
+    padding: 20px;
+  }
+
+  .day{
+    color: #74D9B1;
+    font-size: 40px;
+    margin-bottom: 0px;
+  }
+
+  .date{
+    color: white;
+    font-size: 20px;
+  }
+</style>
